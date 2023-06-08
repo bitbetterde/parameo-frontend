06029adcb054a18c2b4c6a3d8876757d750b30df
Agustina Carrion <agus.carrion@gmail.com>
2023-06-08 13:28:09 +0200

    feat: add counter in tabs

--- src/components/Tabs.tsx
+++ src/components/Tabs.tsx
@@ -1,6 +1,6 @@
 interface Props {
   className?: string;
-  tabs: Array<string>;
+  tabs: Array<{ label: string; length: number }>;
   activeTab: string;
   onClickTab: (tab: string) => void;
 }
@@ -15,17 +15,17 @@ const Tabs: React.FC<Props> = ({ className, tabs, activeTab, onClickTab }) => {
     >
       {tabs.map((tab) => (
         <button
-          key={tab}
+          key={tab.label}
           className={`${
-            activeTab === tab
+            activeTab === tab.label
               ? "bg-indigo-100 text-indigo-700"
               : "text-gray-500 hover:text-gray-700"
           }
           rounded-md px-3 py-2 text-sm font-medium
         `}
-          onClick={() => onClickTab(tab)}
+          onClick={() => onClickTab(tab.label)}
         >
-          {tab}
+          {tab.label} ({tab.length})
         </button>
       ))}
     </nav>
--- src/pages/FaqPage.tsx
+++ src/pages/FaqPage.tsx
@@ -1,7 +1,7 @@
 import { useState, useRef, useEffect } from "react";
 import { HeroSection, ImageSlider, Tabs, Accordion } from "@components";
 import rawFaqs from "@faqs";
-import { IFaqFile } from "@interfaces/IFaq";
+import { groupBy } from "lodash-es";
 
 interface Props {
   itemFromPath?: string;
@@ -31,15 +31,12 @@ const FaqPage: React.FC<Props> = ({ itemFromPath }) => {
   const [activeTab, setActiveTab] = useState<string>("All");
   const openAccordionRef = useRef<HTMLDetailsElement>(null);
 
-  const filterFaqs = (rawFaqs: IFaqFile[], activeTab: string) => {
-    if (activeTab && activeTab !== "All") {
-      return rawFaqs.filter((faq) => faq.tags.includes(activeTab));
-    } else {
-      return rawFaqs;
-    }
-  };
+  const deduplicatedByTags = rawFaqs.flatMap((faq) => [
+    ...faq.tags.map((tag) => ({ ...faq, tags: tag })),
+    { ...faq, tags: "All" },
+  ]);
 
-  const filteredFaqs = filterFaqs(rawFaqs, activeTab);
+  const groupedByTags = groupBy(deduplicatedByTags, (faq) => faq.tags);
 
   const selectTags = (clickedTab: string) => {
     if (clickedTab === activeTab) {
@@ -48,11 +45,9 @@ const FaqPage: React.FC<Props> = ({ itemFromPath }) => {
       setActiveTab(clickedTab);
     }
   };
-
-  const uniqueTags = [
-    "All",
-    ...new Set(rawFaqs.flatMap((tag) => tag.tags).sort()),
-  ];
+  const uniqueTags = Object.entries(groupedByTags)
+    .map((tag) => ({ label: tag[0], length: tag[1].length }))
+    .sort((a, b) => a.label.localeCompare(b.label));
 
   useEffect(() => {
     if (itemFromPath) {
@@ -70,16 +65,18 @@ const FaqPage: React.FC<Props> = ({ itemFromPath }) => {
       />
       <Tabs tabs={uniqueTags} activeTab={activeTab} onClickTab={selectTags} />
       <div className="border-b-2 border-gray-200 mx-auto max-w-7xl lg:w-[768px]" />
-      {filteredFaqs.map((faq) => (
-        <Accordion
-          ref={itemFromPath === faq.id ? openAccordionRef : undefined}
-          key={faq.id}
-          title={faq.title}
-          href={"hello"}
-          children={faq.body}
-          open={itemFromPath === faq.id}
-        />
-      ))}
+      {activeTab &&
+        groupedByTags &&
+        groupedByTags[activeTab].map((faq) => (
+          <Accordion
+            ref={itemFromPath === faq.id ? openAccordionRef : undefined}
+            key={faq.id}
+            title={faq.title}
+            href={"hello"}
+            children={faq.body}
+            open={itemFromPath === faq.id}
+          />
+        ))}
       <ImageSlider items={images} />
     </>
   );
