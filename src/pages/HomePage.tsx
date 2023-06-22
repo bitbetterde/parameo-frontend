d54544cbeb7b2d897f1c17bee5572d87decf920e
Oliver Schwartz <mail@schwartzo.com>
2023-06-22 08:53:33 +0000

    Update file HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -76,7 +76,7 @@ const cards = [
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto accusantium praesentium eius, ut atque fuga culpa, similique sequi cum eos quis dolorum.",
     licence: "licence type",
     cardImage:
-      "https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80",
+      "/images/box-preview-lo.jpg",
     author: {
       name: "regenholz",
       href: "#",
