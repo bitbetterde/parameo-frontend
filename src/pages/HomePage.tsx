52c7b129df8db3ba66d71306cdadc753f79a8a7a
Oliver Schwartz <mail@schwartzo.com>
2023-06-28 09:22:45 +0000

    home feat icons

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -46,25 +46,25 @@ const features = [
     name: "CO2 footprint",
     description:
       "Morbi viverra dui mi arcu sed. Tellus semper adipiscing suspendisse semper morbi. Odio urna massa nunc massa.",
-    icon: "GlobeAltIcon",
+    icon: "light-bulb",
   },
   {
     name: "Material price",
     description:
       "Sit quis amet rutrum tellus ullamcorper ultricies libero dolor eget. Sem sodales gravida quam turpis enim lacus amet.",
-    icon: "BoltIcon",
+    icon: "presentation-chart-line",
   },
   {
     name: "Bill of materials",
     description:
       "Quisque est vel vulputate cursus. Risus proin diam nunc commodo. Lobortis auctor congue commodo diam neque.",
-    icon: "ScaleIcon",
+    icon: "list-bullet",
   },
   {
     name: "DYI or BUY",
     description:
       "Arcu egestas dolor vel iaculis in ipsum mauris. Tincidunt mattis aliquet hac quis. Id hac maecenas ac donec pharetra eget.",
-    icon: "ChatBubbleBottomCenterTextIcon",
+    icon: "cursor-arrow-rays",
   },
 ];
 
