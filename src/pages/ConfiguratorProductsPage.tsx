f73d858e51089ce6784ef4d484e8a720bf368323
Oliver Schwartz <mail@schwartzo.com>
2023-06-28 09:46:18 +0000

    hero img update config page

--- src/pages/ConfiguratorProductsPage.tsx
+++ src/pages/ConfiguratorProductsPage.tsx
@@ -30,7 +30,7 @@ const ConfiguratorProductsPage = () => {
       <HeroSection
         title="Configurator headline here ..."
         subtitle="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum."
-        heroImage="https://images.unsplash.com/photo-1521737852567-6949f3f9f2b5?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2830&q=80&sat=-100"
+        heroImage="/images/parameo-hero-img_configurator.jpg"
         heroImageAlt="People working on laptops"
       />
       {isLoading ? (
