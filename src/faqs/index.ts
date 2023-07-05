306b5d1aac2f7697aac198b81117ff121227788b
Oliver Schwartz <mail@schwartzo.com>
2023-07-05 13:06:02 +0000

    Update index.ts

--- src/faqs/index.ts
+++ src/faqs/index.ts
@@ -1,5 +1,9 @@
 import fabulaser from "./fabulaser";
 import maslowcnc from "./maslowcnc";
 import example from "./example";
+import designer collaboration from "./designer-collaboration";
+import developer collaboration from "./developer-collaboration";
+import manufacturer collaboration from "./manufacturer-collaboration";
+import maker collaboration from "./maker-collaboration";
 
 export default [fabulaser, maslowcnc, example];
