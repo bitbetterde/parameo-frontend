6dd14b94713ce92548d28e79cc6538565fc1c5fc
Agustina Carrion <agus.carrion@gmail.com>
2023-07-18 08:27:21 +0000

    feat: add untitled icons to feature items

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -49,25 +49,25 @@ const features = [
     name: "CO2 footprint",
     description:
       "Morbi viverra dui mi arcu sed. Tellus semper adipiscing suspendisse semper morbi. Odio urna massa nunc massa.",
-    icon: "LightBulbIcon",
+    icon: "globe",
   },
   {
     name: "Material price",
     description:
       "Sit quis amet rutrum tellus ullamcorper ultricies libero dolor eget. Sem sodales gravida quam turpis enim lacus amet.",
-    icon: "PresentationChartLineIcon",
+    icon: "lightning",
   },
   {
     name: "Bill of materials",
     description:
       "Quisque est vel vulputate cursus. Risus proin diam nunc commodo. Lobortis auctor congue commodo diam neque.",
-    icon: "ListBulletIcon",
+    icon: "scale",
   },
   {
     name: "DYI or BUY",
     description:
       "Arcu egestas dolor vel iaculis in ipsum mauris. Tincidunt mattis aliquet hac quis. Id hac maecenas ac donec pharetra eget.",
-    icon: "CursorArrowRaysIcon",
+    icon: "annotation",
   },
 ];
 
