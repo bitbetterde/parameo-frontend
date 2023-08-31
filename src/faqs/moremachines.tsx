73dec0a9e1b0e6fe3997eeae688569fad4f956c4
René Watral <rene@regenholz.de>
2023-08-31 15:09:14 +0000

    Add "more machine integration" faq

---
+++ src/faqs/moremachines.tsx
@@ -0,0 +1,33 @@
+import { IFaqFile } from "@interfaces/IFaq";
+
+const body = (
+  <>
+    <p>
+      Developed with the aim to lower the boundaries of accessing laser cutting technology, Fabulaser Mini represents an ideal compromise between performance and cost. The optimized 40W CO2 laser has a cutting area of 600x400mm, a compact machine size of 870x810x380mm and weight under 60kg.
+    </p>
+    <p>
+    <b> Work area max height:</b> 40cm </p>
+    <p>
+    <b> Work area max width:</b> 60cm </p>
+    <p>
+    <a href="https://www.inmachines.net/Fabulasermini" target="_blank">Project URL</a></p>
+    <p>
+    <a href="http://fabulaser.net/" target="_blank">Documentation URL</a></p>
+    <p>
+    <a href="https://github.com/fab-machines/Fabulaser-Mini" target="_blank">Repository URL</a></p>
+
+    <img src="/images/forrest.jpg" alt="" />
+    
+  </>
+);
+
+export default {
+  title: "Fabulaser MINI",
+  id: "fabulaser",
+  tags: ["Machinery"],
+  body,
+  teaser:
+    "The Fabulaser MINI is an open-source 40W C02 laser cutter representing an ideal compromise between performance and cost.",
+  showOnHomePage: true,
+  order: 2
+} as IFaqFile;
