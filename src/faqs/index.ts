6f580f4829b75f10f2ccfb1936906a21959b6724
René Watral <rene@regenholz.de>
2023-08-31 12:26:19 +0000

    Update index.ts

--- src/faqs/index.ts
+++ src/faqs/index.ts
@@ -1,10 +1,8 @@
 import fabulaser from "./fabulasermini";
 import maslowcnc from "./maslowcnc";
-import example from "./example";
 import designerCollaboration from "./designer-collaboration";
 import developerCollaboration from "./developer-collaboration";
 import manufacturerCollaboration from "./manufacturer-collaboration";
 import makerCollaboration from "./maker-collaboration";
-import fackFaq from "./fack-faq";
 
-export default [fabulaser, maslowcnc, example, designerCollaboration,developerCollaboration, manufacturerCollaboration, makerCollaboration, fackFaq];
+export default [fabulaser, maslowcnc, designerCollaboration,developerCollaboration, manufacturerCollaboration, makerCollaboration];
