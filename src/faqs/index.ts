e47163de867e35ed6a763e7bda3d811f0c32e5b3
René Watral <rene@regenholz.de>
2023-08-31 15:11:28 +0000

    Update index.ts

--- src/faqs/index.ts
+++ src/faqs/index.ts
@@ -4,5 +4,6 @@ import designerCollaboration from "./designer-collaboration";
 import developerCollaboration from "./developer-collaboration";
 import manufacturerCollaboration from "./manufacturer-collaboration";
 import makerCollaboration from "./maker-collaboration";
+import moremachines from "./moremachines";
 
-export default [fabulaser, maslowcnc, designerCollaboration,developerCollaboration, manufacturerCollaboration, makerCollaboration];
+export default [fabulaser, maslowcnc, designerCollaboration, developerCollaboration, manufacturerCollaboration, makerCollaboration, moremachines];
