0120e561f8a7214fa1ecbf902e5300b93e94a03d
René Watral <rene@regenholz.de>
2023-08-31 11:33:11 +0000

    Update maslowcnc.tsx

--- src/faqs/maslowcnc.tsx
+++ src/faqs/maslowcnc.tsx
@@ -2,19 +2,21 @@ import { IFaqFile } from "@interfaces/IFaq";
 
 const body = (
   <>
-    <h2>Description</h2>
+    <h3>Description:</h3>
     <p>
       The Maslow CNC is a large, affordable and space-saving open-source CNC milling machine that mills useful things out of wood and other sheet materials. Since 2017, craftspeople and makers around the world have been working with the Maslow CNC. The growing community shares designs, data and experiences.
     </p>
     <img src="/images/forrest.jpg" alt="" />
-    <h2>Specs</h2>
-    <b> Work area max height:</b> 100cm
-    <b> Work area max width:</b> 180cm
-    <h2>Links</h2>
+    <h3>Specs</h3>
     <p>
-      Project URL: https://www.maslowcnc.com/
-      Documentation URL: https://gitlab.fabcity.hamburg/hardware/maslowcnc
-      Repository URL: https://github.com/makermadecnc
+    <b> Work area max height:</b> 100cm<br>
+    <b> Work area max width:</b> 180cm
+    </p>
+    <h3>Links:</h3>
+    <p>
+      <b>Project URL:</b> https://www.maslowcnc.com/ <br>
+      <b>Documentation URL:</b> https://gitlab.fabcity.hamburg/hardware/maslowcnc <br>
+      <b>Repository URL:</b> https://github.com/makermadecnc <br>
     </p>
   </>
 );
