bce4e5892c507114ab0b3916beb114b2ed9ddd86
René Watral <rene@regenholz.de>
2023-09-05 02:32:59 +0000

    Update co2emission.tsx

--- src/faqs/co2emission.tsx
+++ src/faqs/co2emission.tsx
@@ -3,11 +3,16 @@ import { IFaqFile } from "@interfaces/IFaq";
 const body = (
   <>
   Your individual product life cycle assesment is based on ISO 14000 Environmental Managementstandards, EN 15804 European Standard for Environmental Product Declarations (EDP) in the construction industry and PAS 2050 GHG Protocol - Carbon Footprinting.
+  1. material consumption 
+  2. manufacturing energy
+  3. finish
     <p>
    Sources:
-   - Raw material:
-   - Maschine Run Time:
-   - Operating Ressources: 
+   - Data: PROBAS (Umwelt Bundesamt)
+   - KNOWHOW: ECOCHAIN 
+
+   ... 
+   
     </p>
 
     <img src="/images/forrest.jpg" alt="" />
