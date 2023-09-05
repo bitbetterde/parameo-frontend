a68159daff824098a97e09ec122a8d8648098fb1
Oliver Schwartz <mail@schwartzo.com>
2023-09-05 11:56:47 +0000

    Update openbox.tsx

--- src/faqs/openbox.tsx
+++ src/faqs/openbox.tsx
@@ -7,17 +7,11 @@ const body = (
       The "OpenBox" by regenholz was designed as a simple, stable, portable and stackable box for use in the workshop.  As a whole or with simple divider inserts, the box can be used for storage and transport of different things.
     </p>
     <img src="/images/regenholz-parameo_OpenBox_pic-02.webp" alt="" />
-    <h2>Specs</h2>
+    <h2>Specs / Links</h2>
     <ol>
-      <li>Integer varius imperdiet sed interdum felis cras in nec nunc.</li>
-      <li>Quam malesuada odio ut sit egestas. Elementum at porta vitae.</li>
+      <li>...</li>
+      <li>...</li>
     </ol>
-    <p>
-      Erat pellentesque dictumst ligula porttitor risus eget et eget. Ultricies
-      tellus felis id dignissim eget. Est augue maecenas risus nulla ultrices
-      congue nunc tortor. Eu leo risus porta integer suspendisse sed sit ligula
-      elit.
-    </p>
   </>
 );
 
