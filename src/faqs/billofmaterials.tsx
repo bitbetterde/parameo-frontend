f01d2eea9ead348c35e8f8e208d72cb27461a685
René Watral <rene@regenholz.de>
2023-09-05 00:40:08 +0000

    Update billofmaterials.tsx

--- src/faqs/billofmaterials.tsx
+++ src/faqs/billofmaterials.tsx
@@ -3,9 +3,8 @@ import { IFaqFile } from "@interfaces/IFaq";
 const body = (
   <>
     <p>
-      A BOM is generated in .csv format listing needed ressources for custom production by material choice & machine run time complementing  product specific assembly necessities. 
-      The Input of values like raw material price in €/m3 must be updated manually atm via the admin panel logged by a timestamp.  
-    Source links are accessible and unconditionally adaptive via the admin panel. 
+      A BOM is generated in .csv format listing needed ressources for custom production like material & machine run time, complementing product specific assembly necessities. 
+    Source links are obligatory accessible and unconditionally adaptive via the admin panel. 
     </p>
     <p>
     
