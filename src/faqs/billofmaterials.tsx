52cd54dbd167a36f1c4f9a29fd336a61ee520aa9
René Watral <rene@regenholz.de>
2023-09-05 01:03:48 +0000

    Update billofmaterials.tsx

--- src/faqs/billofmaterials.tsx
+++ src/faqs/billofmaterials.tsx
@@ -4,10 +4,7 @@ const body = (
   <>
     <p>
       A BOM.csv is providing a list of needed resources for custom production like materials and tools, complementing product specific assembly necessities. 
-    Source links are obligatory accessible and unconditionally adaptable via the admin panel.
     </p>
-    <p>
-    
 
     <img src="/images/forrest.jpg" alt="" />
     
