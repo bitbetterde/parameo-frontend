5f4e4a4738d45277d969764214cec22b8382d346
René Watral <rene@regenholz.de>
2023-09-06 11:11:47 +0000

    Update materialprice.tsx

--- src/faqs/materialprice.tsx
+++ src/faqs/materialprice.tsx
@@ -4,7 +4,7 @@ const body = (
   <>
     <p>
       The material impact is valued in kg/m3 co2 emissions ecologically and in
-      €/m3 sheet pricing monetary. Source links are obligatory accessible and
+      €/m3 sheet pricing monetary. References are accessible and
       manually adaptable via the admin panel, logged by a timestamp. Indicators
       are visualized on individual result page.
     </p>
