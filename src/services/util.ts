f8084e1f2f635ddf277cab12b44c764e639db6de
Moritz Stückler <moritz@bitbetter.de>
2023-09-11 15:13:46 +0200

    fix: fetch wrapper

--- src/services/util.ts
+++ src/services/util.ts
@@ -1,12 +1,18 @@
-export const commonHeaderJson: HeadersInit = {
+export const commonHeaders: HeadersInit = {
   "content-type": "application/json",
 };
 
-export const fetchAndHandleErrors = async (request: RequestInfo) => {
-  const response = await fetch(request, { headers: commonHeaderJson });
+export const fetchAndHandleErrors = async <T>(
+  request: RequestInfo,
+): Promise<T> => {
+  const response = await fetch(request, {
+    headers: {
+      ...commonHeaders,
+    },
+  });
 
   if (!response.ok) {
-    throw new Error("Server returned Error: ");
+    throw new Error("Server returned Error: " + (await response.text()));
   }
 
   return response.json();
