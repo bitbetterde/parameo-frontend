0cc9e79cf1736575396e64558d2c661cd2697edc
Oliver Schwartz <mail@schwartzo.com>
2023-09-21 16:19:55 +0000

    Update feedback.tsx

--- src/faqs/feedback.tsx
+++ src/faqs/feedback.tsx
@@ -1,1 +1,27 @@
+import type { IFaqFile } from "@interfaces";
 
+const body = (
+  <>
+    <p>
+      We are happy about your feedback – please use our user feedback form:
+    </p>
+    <p>
+      <a href="https://www.parameo.de/contact" target="_blank">
+        Feedback form
+      </a>
+    </p>
+
+    <img src="/images/faq-addmoremachines.webp" alt="cnc machines" />
+  </>
+);
+
+export default {
+  title: "Feedback",
+  id: "feedback",
+  tags: [""],
+  body,
+  teaser:
+    "User feedback",
+  showOnHomePage: false,
+  order: 3,
+} as IFaqFile;
