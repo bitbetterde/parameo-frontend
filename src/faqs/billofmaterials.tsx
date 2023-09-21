557cffc3fdee6db91cc973a9c7f33c2909538fea
Oliver Schwartz <mail@schwartzo.com>
2023-09-21 15:57:30 +0000

    Update billofmaterials.tsx - Ollie merging "Bom"+"Pricing"+"Add materials" in one FAQ-entry

--- src/faqs/billofmaterials.tsx
+++ src/faqs/billofmaterials.tsx
@@ -2,16 +2,28 @@ import type { IFaqFile } from "@interfaces";
 
 const body = (
   <>
+    <h2>Bill of materials</h2>
     <p>
       A BOM.csv provides a list of needed resources based on configured product
       dimensions for custom production like materials and tools, complementing
       product specific assembly necessities.
     </p>
+    <h2>Material pricing</h2>
+    <p>
+      The material impact is valued in kg/m3 co2 emissions ecologically and in
+      €/m3 sheet pricing monetary. References are accessible and manually
+      adaptable via the admin panel, logged by a timestamp. Indicators are
+      visualized on individual result page.
+    </p>
+    <h2>Material database</h2>
+    <p>
+      The offerd materials are tested in manufacturing for each spectific cnc-machine. For the integration of additional materials, both the maximum working area (and corresponding availability of plate material) and the power of the cnc milling machine used are relevant. only in this way can a correct calculation of the parts and machine paths be calculated. to add additional materials to the database, please contact:
+    </p>
   </>
 );
 
 export default {
-  title: "Bill of Materials",
+  title: "Materials",
   id: "bom",
   tags: ["Manufacturing"],
   body,
