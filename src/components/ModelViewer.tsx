bae868b81bfb1d49d352371b87b103c7178fd5ec
Fabian Schmidt <fabian@bitbetter.de>
2023-09-28 14:27:20 +0000

    add outline to model

--- src/components/ModelViewer.tsx
+++ src/components/ModelViewer.tsx
@@ -1,4 +1,5 @@
 import "@google/model-viewer";
+import "@google/model-viewer-effects";
 import type React from "react";
 import { useEffect, useRef } from "react";
 import type { ModelViewerElement } from "@google/model-viewer/lib/model-viewer";
@@ -18,8 +19,8 @@ const ModelViewer: React.FC<Props> = ({ className, modelSrc, modelAlt }) => {
       material?.pbrMetallicRoughness?.setBaseColorFactor([
         0.866, 0.541, 0.235, 1,
       ]);
-      material?.pbrMetallicRoughness?.setMetallicFactor(0.1);
-      material?.pbrMetallicRoughness?.setRoughnessFactor(0.3);
+      material?.pbrMetallicRoughness?.setMetallicFactor(0.7);
+      material?.pbrMetallicRoughness?.setRoughnessFactor(0.6);
     });
   }, [viewer]);
 
@@ -31,8 +32,17 @@ const ModelViewer: React.FC<Props> = ({ className, modelSrc, modelAlt }) => {
       alt={modelAlt}
       camera-controls
       auto-rotate
+      ar
       class={`w-full h-[540px] rounded-md ${className || ""}`}
-    />
+    >
+      {/*// @ts-expect-error test*/}
+      <effect-composer render-mode="quality">
+        {/*// @ts-expect-error test*/}
+        <outline-effect smoothing={0.4} color="black" strength={1.5} />
+        {/*// @ts-expect-error test*/}
+      </effect-composer>
+      {/*// @ts-expect-error test*/}
+    </model-viewer>
   );
 };
 
