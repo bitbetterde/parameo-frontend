b25d79f967739abb151427ad35ada76e0bd2437d
René Watral <rene@regenholz.de>
2023-09-28 18:59:52 +0000

    Update moremachines.tsx

--- src/faqs/moremachines.tsx
+++ src/faqs/moremachines.tsx
@@ -8,18 +8,13 @@ const body = (
       open-source CNC machine, please contact us directly via the contact form.
       Together we will integrate and document the process.
     </p>
-    <p>
-      <a href="https://www.parameo.de/contact" target="_self">
-        Connect Here
-      </a>
-    </p>
 
     <img src="/images/faq-addmoremachines.webp" alt="cnc machines" />
   </>
 );
 
 export default {
-  title: "Add More Machines",
+  title: "Add more machines",
   id: "moremachines",
   tags: ["Machinery"],
   body,
