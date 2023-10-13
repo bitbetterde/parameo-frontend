88485efa741fb866dd59c0e9c335cbf25d2269cd
René Watral <rene@regenholz.de>
2023-10-13 20:14:12 +0000

    Update materials.tsx

--- src/faqs/materials.tsx
+++ src/faqs/materials.tsx
@@ -4,8 +4,10 @@ const body = (
   <>
     <h2>Material database</h2>
     <p>
-      Material dimensions may differ by availability and location-dependent merchants. <b>Please adapt sheet size for minimal offcuts.</b> Also the pricing may differ by market development and location-dependend sources. <br/>
-To integrate additional materials, both the maximum working area and the power of the cnc machine are significant and should be adjusted via the admin panel to ensure correct calculations of product parts and machine paths. <br/>
+      Material dimensions may differ by availability and location-dependent merchants. <b>Please adapt sheet size for minimal offcuts.</b> Also the pricing may differ by market development and location-dependend sources. </p>
+      <p>
+To integrate additional materials, both the maximum working area and the power of the cnc machine are significant and should be adjusted via the admin panel to ensure correct calculations of product parts and machine paths. </p>
+<p>
 The offered materials are tested for manufacturing with featured CNC machinery. Provided options for materials and their sources are independent suggestions and are <b>easy to change via the admin panel.</b> 
     </p>
     
