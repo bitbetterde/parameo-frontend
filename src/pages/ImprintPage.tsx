e9cb7bccede0c8c4cf8f210174c33e17dd0214fd
Oliver Schwartz <mail@schwartzo.com>
2023-10-17 09:12:52 +0000

    Update ImprintPage.tsx

--- src/pages/ImprintPage.tsx
+++ src/pages/ImprintPage.tsx
@@ -30,64 +30,15 @@ const ImprintPage: React.FC<Props> = ({
       )}
       <div className="pt-6 max-w-none prose prose-h2:text-[30px] prose-h2:leading-10 prose-lg prose-p:leading-8 prose-a:text-indigo-600 text-gray-500">
         <p>
+        <p>
+        <h2>Everything you need to get up and running</h2>
+        </p>
           Faucibus commodo massa rhoncus, volutpat. <strong>Dignissim</strong>{" "}
           sed <strong>eget risus enim</strong>. Mattis mauris semper sed amet
           vitae sed turpis id. Id dolor praesent donec est. Odio penatibus risus
           viverra tellus varius sit neque erat velit. Faucibus commodo massa
           rhoncus, volutpat. Dignissim sed eget risus enim.{" "}
           <a href="#">Mattis mauris semper</a> sed amet vitae sed turpis id.
-        </p>
-        <ul>
-          <li>Quis elit egestas venenatis mattis dignissim.</li>
-          <li>Cras cras lobortis vitae vivamus ultricies facilisis tempus.</li>
-          <li>Orci in sit morbi dignissim metus diam arcu pretium.</li>
-        </ul>
-        <p>
-          Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus
-          enim. Mattis mauris semper sed amet vitae sed turpis id. Id dolor
-          praesent donec est. Odio penatibus risus viverra tellus varius sit
-          neque erat velit. Faucibus commodo massa rhoncus, volutpat. Dignissim
-          sed eget risus enim. Mattis mauris semper sed amet vitae sed turpis
-          id.
-        </p>
-        <h2>From beginner to expert in 30 days</h2>
-        <p>
-          Id orci tellus laoreet id ac. Dolor, aenean leo, ac etiam consequat
-          in. Convallis arcu ipsum urna nibh. Pharetra, euismod vitae interdum
-          mauris enim, consequat vulputate nibh. Maecenas pellentesque id sed
-          tellus mauris, ultrices mauris. Tincidunt enim cursus ridiculus mi.
-          Pellentesque nam sed nullam sed diam turpis ipsum eu a sed convallis
-          diam.
-        </p>
-        <blockquote>
-          “Sagittis scelerisque nulla cursus in enim consectetur quam. Dictum
-          urna sed consectetur neque tristique pellentesque. Blandit amet, sed
-          aenean erat arcu morbi.”
-        </blockquote>
-        <p>
-          Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus
-          enim. Mattis mauris semper sed amet vitae sed turpis id. Id dolor
-          praesent donec est. Odio penatibus risus viverra tellus varius sit
-          neque erat velit.
-        </p>
-        <img src="/images/forrest.jpg" alt="Image of a forrest" />
-        <figcaption>
-          Sagittis scelerisque nulla cursus in enim consectetur quam.
-        </figcaption>
-        <h2>Everything you need to get up and running</h2>
-        <p>
-          Purus morbi dignissim senectus mattis <a href="#">adipiscing</a>.
-          Amet, massa quam varius orci dapibus volutpat cras. In amet eu
-          ridiculus leo sodales cursus tristique. Tincidunt sed tempus ut
-          viverra ridiculus non molestie. Gravida quis fringilla amet eget dui
-          tempor dignissim. Facilisis auctor venenatis varius nunc, congue erat
-          ac. Cras fermentum convallis quam.
-        </p>
-        <p>
-          Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus
-          enim. Mattis mauris semper sed amet vitae sed turpis id. Id dolor
-          praesent donec est. Odio penatibus risus viverra tellus varius sit
-          neque erat velit.
         </p>
       </div>
     </div>
